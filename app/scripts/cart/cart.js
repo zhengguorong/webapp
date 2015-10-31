ccb2b2dc7f6085bb3dc73b68469f76d2ffd3e031
zhengguorong <zhengguorong@bluemoon.com.cn>
2015-10-31 03:26:56 -0700

    加入通用组件

--- app/scripts/cart/cart.js
+++ app/scripts/cart/cart.js
@@ -2,8 +2,6 @@ var CartModel=function(){
 	var self=this;
 	self.title="this is a cart";
   self.showAlert=function(){
-    app.plugin.alert.show("我是标题","我是内容",function(){
-      console.log("callback")
-    })
+    app.popup.alert("12","22",null,null)
   }
 }
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,26 +1,32 @@
-var app={};
+
+var app={
+  launch:function(){
+    this.popup.init();
+    this.toast.init();
+  }
+};
 
 /**
  * 页面路由功能
  */
 app.router = $.sammy(function(){
   this.get("#/home",function(){
-      app.showPage("indexPage",null,function(){
-        app.indexModel = new IndexModel();
-        app.renderTemplate("views/home/index.html",app.indexModel,"indexPage");
-      });
+    app.showPage("indexPage",null,function(){
+      app.indexModel = new IndexModel();
+      app.renderTemplate("views/home/index.html",app.indexModel,"indexPage");
+    });
   })
   this.get("#/cart",function(){
-      app.showPage("cartPage",null,function(){
-        app.cartModel = new CartModel();
-        app.renderTemplate("views/cart/cart.html",app.cartModel,"cartPage");
-      });
+    app.showPage("cartPage",null,function(){
+      app.cartModel = new CartModel();
+      app.renderTemplate("views/cart/cart.html",app.cartModel,"cartPage");
+    });
   })
   this.get("#/category",function(){
-      app.showPage("categoryPage",null,function(){
-        app.categoryModel = new CategoryModel();
-        app.renderTemplate("views/cate/category.html",app.categoryModel,"categoryPage");
-      });
+    app.showPage("categoryPage",null,function(){
+      app.categoryModel = new CategoryModel();
+      app.renderTemplate("views/cate/category.html",app.categoryModel,"categoryPage");
+    });
   })
 
 })
@@ -33,10 +39,10 @@ app.router = $.sammy(function(){
  * @return {null}
  */
 app.renderTemplate=function(templateUrl,controller,pageId){
-	$.get(templateUrl,function(data){
-		$("#appView").append(data);
-		ko.applyBindings(controller,document.getElementById(pageId));
-	})
+  $.get(templateUrl,function(data){
+    $("#appView").append(data);
+    ko.applyBindings(controller,document.getElementById(pageId));
+  })
 }
 
 /**
@@ -46,100 +52,212 @@ app.renderTemplate=function(templateUrl,controller,pageId){
  * @return {null}
  */
 app.showPage=function(pageId,successCallback,fallCallback){
-    $("#appView section").hide();
-    if(document.getElementById(pageId)){
-      if(successCallback){
-        successCallback();
-      }else{
-        $("#"+pageId).show();
-      }
+  $("#appView section").hide();
+  if(document.getElementById(pageId)){
+    if(successCallback){
+      successCallback();
     }else{
-      if(fallCallback){
-        fallCallback();
-      }
+      $("#"+pageId).show();
     }
+  }else{
+    if(fallCallback){
+      fallCallback();
+    }
+  }
 }
-
-app.plugin=(function(){
-  function AlertModel(){
+/**
+ 弹出框组建
+ */
+app.popup=(function(){
+  var Alert=function(){
     var self=this;
     self.title=ko.observable();
     self.content=ko.observable();
-    self.confirm;
-    self.show=function(title,content,successCallback){
-       $("#alert").addClass("active");
-       if(successCallback){
-         self.confirm=successCallback;
-       }else{
-         self.hide();
-       }
-    }
-    self.hide=function(){
-      $("#alert").removeClass("active");
-    }
+    self.btnName=ko.observable();
   }
-  function ConfirmModel(){
+  var Confirm=function(){
     var self=this;
     self.title=ko.observable();
     self.content=ko.observable();
-    self.confirm;
-    self.cancel;
-    self.show=function(title,content,successCallback,failCallback){
-      $("#confirm").addClass("active");
-      if(successCallback){
-        self.confirm=successCallback;
-      }else{
-        self.hide();
-      }
-      if(failCallback){
-        failCallback()
-      }else{
-        self.hide();
-      }
-    }
-    self.hide=function(){
-      $("#confirm").removeClass("active");
+  }
+  var bmAlert=new Alert();
+  var bmConfirm=new Confirm();
+  /**
+   *alert 组件
+   *@param title 标题
+   *@param content 内容
+   *@param btnName 按钮内容
+   */
+  var alert=function(title,content,btnName,okCall,cancelCall){
+    var alertObj=$(".popup.alert");
+    bmAlert.title(title);
+    bmAlert.content(content);
+    bmAlert.btnName(btnName||"确定");
+    show(alertObj);
+    alertObj.find(".confirm").click(function(){
+      hide(alertObj);
+      if(okCall)okCall.call(this);
+    });
+    alertObj.find(".cancel").click(function(){
+      hide(alertObj);
+      if(cancelCall)cancelCall.call(this);
+    });
+  }
+  /**
+   *confirm 组件
+   *@param title 标题
+   *@param content 内容
+   *@param okCall 确认按钮回调
+   *@param cancelCall 取消按钮回调
+   */
+  var confirm=function(title,content,okCall,cancelCall){
+    var confirmObj=$(".popup.confirm");
+    bmConfirm.title(title)
+    bmConfirm.content(content)
+    show(confirmObj);
+    confirmObj.find(".confirm").click(function(){
+      hide(confirmObj);
+      if(okCall)okCall.call(this);
+    });
+    confirmObj.find(".cancel").click(function(){
+      hide(confirmObj);
+      if(cancelCall)cancelCall.call(this);
+    });
+  }
+  var showLoading=function(){
+    var loadingObj=$(".popup.loading");
+    show(loadingObj);
+  }
+  var hideLoading=function(){
+    var loadingObj=$(".popup.loading");
+    loadingBGHide(loadingObj);
+  }
+  var show=function(obj){
+    $(obj).addClass("active");
+    $(".TB_overlayBG").addClass("active");
+  }
+  //隐藏loading的背景
+  var loadingBGHide=function(obj){
+    $(obj).removeClass("active");
+    if($(".popup.active").length==0){
+      $(".TB_overlayBG").removeClass("active");
     }
   }
-
-  var alertModel=new AlertModel();
-  var confirmModel=new ConfirmModel();
-  ko.applyBindings(alertModel,document.getElementById("alert"));
-  ko.applyBindings(confirmModel,document.getElementById("confirm"));
-  return {alert:alertModel,confirm:confirmModel}
+  var hide=function(obj){
+    $(obj).removeClass("active");
+    $(".TB_overlayBG").removeClass("active");
+  }
+  var hideAllPopup=function(){
+    $(".TB_overlayBG.active").removeClass("active");
+    $(".popup.active").removeClass("active");
+  }
+  var init=function(){
+    $(".TB_overlayBG").click(function(){
+      hideAllPopup();
+    })
+    ko.applyBindings(bmAlert, document.getElementById("alert"));
+    ko.applyBindings(bmConfirm, document.getElementById("confirm"));
+  }
+  return{
+    alert:alert,
+    confirm:confirm,
+    showLoading:showLoading,
+    hideLoading:hideLoading,
+    hideAllPopup:hideAllPopup,
+    init:init
+  }
 })();
+
+/*toast提示*/
+app.toast=(function(){
+  var Toast=function(){
+    var self=this;
+    self.content=ko.observable();
+  }
+  var toast=new Toast();
+  var init=function(){
+    toast=new Toast();
+    ko.applyBindings(toast, document.getElementById("toast"));
+  }
+  var defalut=function(content,time,callback){
+    var obj=$(".prompt.toast .pmt_default");
+    _show(obj,content);
+    setTimeout(function(){
+      _hide(obj)
+    },time||1000);
+  }
+  var success=function(content,time,callback){
+    var obj=$(".prompt.toast .pmt_success");
+    _show(obj,content);
+    setTimeout(function(){
+      _hide(obj,callback)
+    },time||1000);
+  }
+  var error=function(content,time,callback){
+    var obj=$(".prompt.toast .pmt_failure");
+    _show(obj,content);
+    setTimeout(function(){
+      _hide(obj)
+    },time||1000);
+  }
+  var info=function(content,time,callback){
+    var obj=$(".prompt.toast .pmt_sigh");
+    _show(obj,content);
+    setTimeout(function(){
+      _hide(obj)
+    },time||1000);
+  }
+  var _hide=function(obj,callback){
+    $(obj).removeClass("active");
+    callback&&callback();
+  }
+  var _show=function(obj,content){
+    toast.content(content);
+    obj.addClass("active");
+  }
+
+  return{
+    defalut:defalut,
+    success:success,
+    error:error,
+    info:info,
+    init:init
+  }
+})();
+
 
 
 
 $(function(){
   app.router.run("#/home");
+  app.launch();
   /* 添加用户窗口事件 改变根字号大小 */
   function addEvent(obj,type,fn){
-      if(obj.addEventListener){
-          obj.addEventListener(type,fn,false);
-      } else {
-          obj.attachEvent('on'+type,fn);
-      }
+    if(obj.addEventListener){
+      obj.addEventListener(type,fn,false);
+    } else {
+      obj.attachEvent('on'+type,fn);
+    }
   }
 
 
   function autoSize(){
-      var font = Math.round(win_W / 16);
-      var HTML=document.getElementById('html');
-      var win_W = $(".container").width();
-      if(win_W>=364 && win_W<=375){
-          font=24;
-      }else{
+    var font = Math.round(win_W / 16);
+    var HTML=document.getElementById('html');
+    var win_W = $(".container").width();
+    if(win_W>=364 && win_W<=375){
+      font=24;
+    }else{
 
-          if( win_W == 360 || win_W == 361){
-              font=22;
-          }else{
-              if( win_W > 540 ){
-                  font=34;
-              }else{ font = Math.round( win_W / 16 ); }
-          }
+      if( win_W == 360 || win_W == 361){
+        font=22;
+      }else{
+        if( win_W > 540 ){
+          font=34;
+        }else{ font = Math.round( win_W / 16 ); }
       }
-      HTML.style.fontSize = font + 'px';
+    }
+    HTML.style.fontSize = font + 'px';
 
 
   }
